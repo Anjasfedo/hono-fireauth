c14081c864f4ec2aff8c7169a7748fb93951f7c1
AnjasFedo <fedoaf98@gmail.com>
2024-05-26 23:08:03 +0700

    Do// Auth Firebase

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase-admin/app";
 import { credential } from "firebase-admin";
+import { getAuth } from "firebase-admin/auth";
 
-const app = initializeApp({ credential: credential.cert()});
+const app = initializeApp({ credential: credential.cert('serviceAccount.json')});
+
+export const auth = getAuth()
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from 'hono'
+import { bearerAuth } from 'hono/bearer-auth'
+import { auth } from './configs/firebase'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.use('/api/*', bearerAuth({
+    verifyToken: async (token, c) => {
+    try {
+      await auth.verifyIdToken(token);
+      
+      return true;
+    } catch (error) {
+      return false;
+    }
+    },
+  }))
+
+app.get('/api/page', (c) => {
+  return c.json({ message: 'You are authorized' })
 })
 
 export default app
